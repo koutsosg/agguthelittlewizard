e3573c9d4403873dd42f70cd81ec808b21637c3f
koutsosg <g.koutsos123@gmail.com>
2020-12-19 20:09:39 +0200

    change favicon name

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/aggu-icon2.png`,
+        icon: `content/assets/agguicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
