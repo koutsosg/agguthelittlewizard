0ce01940a750662ba1e0b33e82b818b9a53d9623
koutsosg <g.koutsos123@gmail.com>
2020-12-19 20:32:22 +0200

    change image location

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/agguicon.png`,
+        icon: `src/Images/agguicon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
