c8c1dc0996ce61fd5d7810a1598f44180b647242
koutsosg <g.koutsos123@gmail.com>
2020-12-19 11:29:15 +0200

    try to correct manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/aggu-icon.png`,
+        icon: `content/assets/aggu-icon2.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
