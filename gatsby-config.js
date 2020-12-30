043b409624ce09bb95ae887886c174f72024da45
koutsosg <g.koutsos123@gmail.com>
2020-12-30 14:09:22 +0200

    small modifications

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-161467146-3",
+        trackingId: "UA-161467146-1",
         head: true,
         anonymize: true,
         respectDNT: true,
